820f7b9e85be94e036faf177da30ac52e381cb9f
Pankaj Parkar <parkarpankaj3@gmail.com>
2021-06-25 00:45:56 +0800

    GDSC bootcamp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-angular';
+
+  showHobbies = true;
+
+  salary = 10000;
+  date = new Date();
+
+  // Bindings
+  // A way of showing/displaying JS variable automatically on page
+  student = {
+    firstName: 'Pankaj',
+    lastName: 'Parkar',
+    age: 30,
+    hobbies: [
+      {value: 'Cricket'},
+      {value: 'Football'},
+      {value: 'Singing'}
+    ]
+  };
+
+  checkboxClick() {
+    if (this.showHobbies) {
+      this.showHobbies = false;
+    } else {
+      this.showHobbies = true;
+    }
+  }
+
+  submit() {
+
+    console.log('Form Should Save');
+  }
+  
 }
